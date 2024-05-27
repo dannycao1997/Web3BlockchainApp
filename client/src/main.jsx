45953f5daa6a7c8ca4f07037422b02a94499ad21
Danny Cao <dannycao1997@yahoo.com>
2024-05-27 08:32:54 -0400

    cleaning up files

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
 import { TransactionsProvider } from "./context/TransactionContext";
+import "./index.css";
 
-const container = document.getElementById("root");
-
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
     <TransactionsProvider>
         <App />
-    </TransactionsProvider>
+    </TransactionsProvider>,
+    document.getElementById("root"),
 );
