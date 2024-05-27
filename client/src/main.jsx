c9b7137b67b82afbd146e98688cb437ffad9f62c
Danny Cao <dannycao1997@yahoo.com>
2024-05-27 05:30:18 -0400

    debugging react-dom issue with react 18

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client'; // Import createRoot
+import App from './App.jsx';
+import './index.css';
 import { TransactionsProvider } from "./context/TransactionContext";
 
+const container = document.getElementById("root");
 
-ReactDOM.render(
+const root = createRoot(container);
+
+root.render(
     <TransactionsProvider>
         <App />
-    </TransactionsProvider>,
-    document.getElementById("root"),
+    </TransactionsProvider>
 );
