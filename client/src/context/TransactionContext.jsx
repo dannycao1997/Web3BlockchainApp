cf99d6c788790ba239515edb5ea86ee3b2d78875
Danny Cao <dannycao1997@yahoo.com>
2024-05-27 05:40:20 -0400

    implementing code for services.jsx

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -8,7 +8,7 @@ export const TransactionContext = React.createContext();
 const { ethereum } = window;
 
 const createEthereumContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
 
