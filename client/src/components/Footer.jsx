5a0a96060ebc712849d8f9db50f96f0491576735
Danny Cao <dannycao1997@yahoo.com>
2024-05-27 23:47:48 -0400

    testing gifs on blockchain transactions

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -18,7 +18,6 @@ const Footer = () => (
         <div className="flex justify-center items-center flex-col mt-5">
             <p className="text-white text-sm text-center">
                 Connect with me on LinkedIn and explore my projects on GitHub :)
-
             </p>
             <div className="mt-2 flex justify-center">
                 <a href="https://www.linkedin.com/in/dannycao97/" target="_blank" rel="noopener noreferrer"
@@ -46,7 +45,7 @@ const Footer = () => (
         <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 "/>
 
         <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-            <p className="text-white text-left text-xs">Dannyverse | Crypto Exchange</p>
+            <p className="text-white text-left text-xs">Dannyverse | Blockchain App</p>
             <p className="text-white text-right text-xs">All rights reserved</p>
         </div>
     </div>
