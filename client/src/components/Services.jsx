7194eaa5c21904a4f2f012fab9100f719597943a
Danny Cao <dannycao1997@yahoo.com>
2024-05-27 23:09:48 -0400

    fixing service cards

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -36,20 +36,20 @@ const Services = () => (
                     color="bg-[#2952E3]"
                     title="Security Guarantee"
                     icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
-                    subtitle="Security is our top priority. We utilize advanced encryption methods and multi-layered security protocols to ensure that all transactions and user data are protected.
-                    Our commitment to privacy is unwavering, and we regularly update our systems to guard against new threats, ensuring that your digital assets are always safe."
+                    subtitle="Security is our top priority. We utilize advanced encryption methods and multi-layered security protocols to ensure that all transactions and user data are protected and
+                    regularly update our systems to guard against new threats, ensuring that your digital assets are always safe."
                 />
                 <ServiceCard
                     color="bg-[#8945F8]"
                     title="Best Exchange Rates"
                     icon={<BiSearchAlt fontSize={21} className="text-white" />}
-                    subtitle="Get more for your money. Our platform constantly monitors global cryptocurrency markets to offer you the best possible exchange rates. By leveraging real-time data analysis and our strategic partnerships, we ensure that you receive the most favorable financial conditions for every transaction, maximizing your investment potential."
+                    subtitle="Get more for your money. By leveraging real-time data analysis and our strategic partnerships, we ensure that you receive the most favorable financial conditions for every transaction, maximizing your investment potential."
                 />
                 <ServiceCard
                     color="bg-[#F84550]"
                     title="Fastest Transactions"
                     icon={<RiHeart2Fill fontSize={21} className="text-white" />}
-                    subtitle="Experience lightning-fast processing. Our state-of-the-art transaction system ensures that your trades are executed almost instantly. With our optimized network infrastructure and cutting-edge technology, we minimize latency and ensure that your transactions go through swiftly, enabling you to take advantage of market movements as they happen."
+                    subtitle="Experience lightning-fast processing. With our optimized network infrastructure and cutting-edge technology, we minimize latency and ensure that your transactions go through swiftly, enabling you to take advantage of market movements as they happen."
                 />
             </div>
         </div>
